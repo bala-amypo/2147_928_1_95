7a8594028a484c26410e261092552a3474385551
bala-amypo <bala@amypo.in>
2025-12-18 09:19:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/BudgetSummary.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+public class BudgetSummary {
+private Long id;
+private Long budgetPlanId;
+private Double totalIncome;
+private Double totalExpense;
+private String status;
+public Long getId() {
+     return id; 
+    }
+public void setId(Long id) { 
+    this.id = id; 
+}
+public Long getBudgetPlanId() { 
+    return budgetPlanId; 
+}
+public void setBudgetPlanId(Long budgetPlanId) { 
+    this.budgetPlanId = budgetPlanId; 
+}
+public Double getTotalIncome() { 
+    return totalIncome; 
+}
+public void setTotalIncome(Double totalIncome) { 
+    this.totalIncome = totalIncome; 
+}
+public Double getTotalExpense() { 
+    return totalExpense; 
+}
+public void setTotalExpense(Double totalExpense) { 
+    this.totalExpense = totalExpense; 
+}
+public String getStatus() { 
+    return status; 
+}
+public void setStatus(String status) { 
+    this.status = status; 
+}
+public BudgetSummary(Long id, Long budgetPlanId, Double totalIncome, Double totalExpense, String status) {
+    this.id = id;
+    this.budgetPlanId = budgetPlanId;
+    this.totalIncome = totalIncome;
+    this.totalExpense = totalExpense;
+    this.status = status;
+}
+public BudgetSummary() {
+
+}
+}
