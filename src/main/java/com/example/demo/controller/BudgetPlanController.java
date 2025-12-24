6ab90fb700e50a9cc78d7e2e904a637f123bc521
bala-amypo <bala@amypo.in>
2025-12-24 05:25:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/BudgetPlanController.java
+++ src/main/java/com/example/demo/controller/BudgetPlanController.java
@@ -1,21 +1,29 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.entity.BudgetPlan;
+
+import com.example.demo.model.BudgetPlan;
 import com.example.demo.service.BudgetPlanService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/budget-plans")
+@RequestMapping("/budgets")
 public class BudgetPlanController {
+
     private final BudgetPlanService budgetPlanService;
+
     public BudgetPlanController(BudgetPlanService budgetPlanService) {
         this.budgetPlanService = budgetPlanService;
     }
+
     @PostMapping("/{userId}")
-    public BudgetPlan createPlan( @PathVariable Long userId, @RequestBody BudgetPlan plan) {
+    public BudgetPlan createBudgetPlan(@PathVariable Long userId,
+                                       @RequestBody BudgetPlan plan) {
         return budgetPlanService.createBudgetPlan(userId, plan);
     }
+
     @GetMapping("/{userId}/{month}/{year}")
-    public BudgetPlan getPlan( @PathVariable Long userId, @PathVariable Integer month, @PathVariable Integer year) {
+    public BudgetPlan getBudgetPlan(@PathVariable Long userId,
+                                    @PathVariable Integer month,
+                                    @PathVariable Integer year) {
         return budgetPlanService.getBudgetPlan(userId, month, year);
     }
 }
